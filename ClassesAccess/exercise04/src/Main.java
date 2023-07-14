5e2ee31b92cf2feabe4085c8f615487195aac674
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-07-14 23:55:33 +0300

    Update Main.java

--- ClassesAccess/exercise04/src/Main.java
+++ ClassesAccess/exercise04/src/Main.java
@@ -1,7 +1,3 @@
-//To ktirio exei mesa orofous
-//o orofos exei mesa diamerismata
-//kai ta diamerismata exoun mesa kapoia pliroforia
-
 class Main {
     public static void main(String[] args) {
         Building b = new Building(3,4);
