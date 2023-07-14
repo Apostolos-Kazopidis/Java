e546e0c177d2fb2796a1a9fabd5c28c8e280e941
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-07-14 23:54:03 +0300

    Update Stack.java

--- ClassesAccess/exercise01/src/Stack.java
+++ ClassesAccess/exercise01/src/Stack.java
@@ -5,33 +5,27 @@ public class Stack {
 
     Stack(){
         array = new int[SIZE];
-        top = -1; //symbolizei oti h stoiva mas einai adeia
-                  //AN JEKINOUSA APO TO 0 THA SIMENE OTI H STOIVA EXEI ENA STOIXEIO MESA HDH
+        top = -1;
     }
 
     public void push(int value) {
-        //SIZE-1 GT JEKINAS APO 0->9 SKEPSOU THESI 8 KANEIS PUSH FTANEIS 9
-        //PREPEI NA VRISKESAI SIZE -1 GIA NA KANEIS PUSH
-        //ALLIWS PROBLIMA
-        if(top<SIZE-1) { //oso to top DEN EINAI GEMATO diladi einai adeio
-            top++; //aujise kata 1
-            array[top]=value; //vale ston pinaka mesa to stoixeio
-                              //array[top] AKRIVWS TO IDIO ME array[i]
+        if(top<SIZE-1) { 
+            top++;
+            array[top]=value;
         }
         else {
-            System.out.println("H stoiva einai gemath.....");
+            System.out.println("Stack is fuil.....");
         }
     }
 
     public int pop(){
-        if(top>=0) { //OSO TO TOP EXEI MESA STOIXEIA DILADI EINAI GEMATO TOTE....
-            top--; //vgale mou to teleutaio stoixeio
-            return array[top + 1]; //kane mou epistrofi to stoixeio pou vgike
-                                   // gia auto allwste kai top +1
+        if(top>=0) {
+            top--;
+            return array[top + 1];
         }
         else {
-            System.out.println("H stoiva einai adeia......");
-            return -1; //mpakalikh gia thn epistrofh KANONIKA PRPEPEI EXCEPTION sygkekrimena to throw!!!
+            System.out.println("Stack is empty.....");
+            return -1;
         }
     }
 
@@ -44,7 +38,7 @@ public class Stack {
 
     /*
     public boolean empty(){
-        return top==-1; //epistrei true alliws FALSE
+        return top==-1;
     * */
 
 
