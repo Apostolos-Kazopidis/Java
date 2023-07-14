a2d4ea4d05a801b55eb1454bca2da8bdbf8f29b4
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-07-15 00:15:23 +0300

    Update Main.java

--- InheritanceMethods/Object/src/Main.java
+++ InheritanceMethods/Object/src/Main.java
@@ -6,7 +6,7 @@ class Main {
         System.out.println(s.toString());
         System.out.println(s);
         SampleClass s2 = new SampleClass(s);
-        System.out.println(s==s2); //sygkrish anaforwn
-        System.out.println(s.equals(s2)); //sygkrish twn timwn pou exoun ta antikeimena
+        System.out.println(s==s2);
+        System.out.println(s.equals(s2));
     }
 }
