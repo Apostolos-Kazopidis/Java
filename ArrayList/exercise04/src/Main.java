7606bce7474eb08feaea278262878b95005804e4
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-07-14 23:49:31 +0300

    Update Main.java

--- ArrayList/exercise04/src/Main.java
+++ ArrayList/exercise04/src/Main.java
@@ -11,15 +11,6 @@ public class Main {
                         new Person("Cersei", "Lannister")
 						};
 
-		/*
-		Person[] array = new Person[3];
-
-		array[0] = new Person("John", "Snow");
-		array[1] = new Person("Tywin", "Lannister");
-		array[2] = new Person("Cersei", "Lannister");
-
-		 */
-
 	    Arrays.sort(array);
 
 	    for (Person p : array)
