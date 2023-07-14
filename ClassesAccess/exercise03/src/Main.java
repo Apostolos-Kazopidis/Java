53b364fe9e185eb9d228657fb2c5c8279ee3bfb1
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-07-14 23:54:57 +0300

    Update Main.java

--- ClassesAccess/exercise03/src/Main.java
+++ ClassesAccess/exercise03/src/Main.java
@@ -1,18 +1,16 @@
 class Main {
     public static void main(String[] args) {
         final int SIZE = 5;
-        //Desmeush pinaka me anafores
+        
         BankAccount[] array = new BankAccount[SIZE];
 
-        //Arxikopoiisi kathe anaforas
         array[0]=new BankAccount("AposKaz","5566544",500.0,5);
         array[1]=new BankAccount("MitsoLar","524154",600.0,6);
         array[2]=new BankAccount("SakisR","4251425",660.0,55);
         array[3]=new BankAccount("RoulaKok","4215124",100.0,2);
         array[4]=new BankAccount("Minases","42512",570.0,45);
 
-        //Synarthsh eureshs max balance
-        double max = -1; //keno gia arxh
+        double max = -1;
         BankAccount m = null;
         for(var account: array){
             if(account.getBalance()>max) {
