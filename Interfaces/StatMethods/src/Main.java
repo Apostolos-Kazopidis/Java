6a88760e114b4b53de66860e968f5fcb59dc16ae
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-07-15 00:18:39 +0300

    Update Main.java

--- Interfaces/StatMethods/src/Main.java
+++ Interfaces/StatMethods/src/Main.java
@@ -3,10 +3,6 @@ class Main {
         SClass s = new SClass();
         s.g();
 
-        //(new SClass()).g();
-        //Idios tropos aplos epeidh den yparxei anafora antikeimenou
-        //sto telos tha to mazepsei o garbage collector
-
         System.out.println(StatInterface.f());
     }
 }
