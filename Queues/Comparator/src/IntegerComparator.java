510382f371e7afd2c8a3e632ce3303db5564b37c
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-07-15 00:28:15 +0300

    Update IntegerComparator.java

--- Queues/Comparator/src/IntegerComparator.java
+++ Queues/Comparator/src/IntegerComparator.java
@@ -3,6 +3,6 @@ import java.util.Comparator;
 public class IntegerComparator implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
-        return o2.compareTo(o1); //Kata fthinousa seira 4 3 2 1 //an evaze o1 kai meta o2 tote tha itan aujousa 1 2 3 4
+        return o2.compareTo(o1); 
     }
 }
