51522d19e8ef3c9d8354b1f236d868b820548ed0
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-07-15 00:21:09 +0300

    Update MyInterface.java

--- Interfaces/exercise03/src/MyInterface.java
+++ Interfaces/exercise03/src/MyInterface.java
@@ -1,21 +1,16 @@
 public interface MyInterface {
-    //int ix; //variable POU DEN MPOREIS NA VALEIS EDW
-    int ix = 0; //static constant-statikh stathera
-    final double PI =3.14159; //static constant-statikh stathera
-    //static int lol; //statikh metabliti den ginetai
-    static int sx =2;//statikh stathera
+    int ix = 0;
+    final double PI =3.14159;
+    static int sx =2;
 
-
-    //method-methodos MONO AN VALOUME THN LEJH default BROSTA
     default void iif() {
 
     }
 
-    //static method
     static void ig() {
 
     }
 
-    void ih(); //abstract method
+    void ih();
 
 }
