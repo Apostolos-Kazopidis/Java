b2eac213478ccdf7255a12fb931d8f5fdb4404d6
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-08-02 18:16:34 +0300

    Delete exercise03.java

--- Arrays/exercise03.java
+++
@@ -1,8 +0,0 @@
-class exercise03 {
-    public static void main(String[] args) {
-        String[] array = {"Leonidas", "Jack", "Alex"};
-
-        for(String names: array)
-            System.out.println(names+" ");
-    }
-}
