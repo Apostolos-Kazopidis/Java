750acb3e1fe5a6901979f8c9e11c9bee5ac629a9
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-08-02 18:16:59 +0300

    Delete forEach.java

--- Arrays/forEach.java
+++
@@ -1,9 +0,0 @@
-class forEach {
-    public static void main(String[] args) {
-        int[] myArray = {1, 2, 3, 4};
-
-        for (int x: myArray)
-            System.out.println(x + " ");
-
-    }
-}
