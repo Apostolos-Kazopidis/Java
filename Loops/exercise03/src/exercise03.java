8d8d776da1918f9ce13d0415c71397afb5048d5d
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-09-04 14:17:07 +0300

    Add files via upload

---
+++ Loops/exercise03/src/exercise03.java
@@ -0,0 +1,14 @@
+class exercise03 {
+    public static void main(String[] args) {
+        char c;
+
+        c=0;
+        while(c<=10000) {
+            System.out.print(c);
+
+            if (c%200==0)
+                System.out.println("");
+            c++;
+        }
+    }
+}
