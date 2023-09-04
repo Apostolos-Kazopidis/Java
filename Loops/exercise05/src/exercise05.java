0269290d7dbc950c18c04928e5c2030fc907a5a5
Apostolos Kazopidis <70575515+Apostolos-Kazopidis@users.noreply.github.com>
2023-09-04 14:17:37 +0300

    Add files via upload

---
+++ Loops/exercise05/src/exercise05.java
@@ -0,0 +1,12 @@
+class exercise05 {
+    public static void main(String[] args) {
+        int i;
+
+        i=0;
+        do {
+            int sq=i*i;
+            System.out.println(i +"^2=" +sq);
+            i++;
+        } while (i<=100);
+    }
+}
---
+++ Loops/exercise08/src/exercise08.java
@@ -0,0 +1,11 @@
+class exercise08 {
+    public static void main(String[] args) {
+        int sum=0;
+
+        for (int i=1; i<=99; i++)
+            if (i%2==1)
+                sum+=i;
+
+        System.out.println("Sum: " + sum);
+    }
+}
